aed5a3570a336ed3e75c5b50ba2fcb934f8e3772
Yash Uppal <2k22.cse.2213230@gmail.com>
2025-07-07 23:18:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.cpp
+++ Difficulty: Basic/Reverse a String/reverse-a-string.cpp
@@ -1,16 +1,10 @@
 // User function Template for C++
 class Solution {
   public:
-     void reverse(string&s,int i,int j){
-      if(i>=j){
-          return;
-      }
-      swap(s[i],s[j]);
-      reverse(s,i+1,j-1);
-  }
-    string reverseString(string& s) {
-        
-        reverse(s,0,s.size()-1);
+    string revStr(string s) {
+        // code here
+        int i = 0, j = s.size()-1;
+        while(i<=j) swap(s[i++],s[j--]);
         return s;
     }
 };
